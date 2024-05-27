03a66a1fcc96e3e26e619c8db4147c520a38e1fb
Mikhail Postnikov <postnikovmu@mail.ru>
2024-05-27 18:01:17 +0300

    test utils.getLinkStats

--- 2-count-clicks-on-links/main.py
+++ 2-count-clicks-on-links/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 
 def shorten_link(token, link):
@@ -31,8 +32,33 @@ def shorten_link(token, link):
 
 def count_clicks(token, link):
 
+    parsed = urlparse(link)
+    key = parsed.path.replace('/', '')
+    print("key:", key)
+
+    url_template = 'https://api.vk.ru/method/utils.getLinkStats'
+    headers = {'Authorization': f'Bearer {token}'}
+    params = {
+        # VK API version
+        'v': '5.236',
+        # Shortened link (part of the URL after “vk.cc/”)
+        'key': key,
+        'extended': 0
+    }
+    url = url_template
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+
+    print(response)
+    print(response.text)
+    print(response.json())
+    api_error_msg = ''
     clicks_count = 0
-    return clicks_count
+    if response.ok and response.json().get("response"):
+        clicks_count = int(response.json().get("response").get("stats")[0].get("views"))
+    if response.ok and response.json().get("error"):
+        api_error_msg = response.json().get("error").get("error_msg")
+    return api_error_msg, clicks_count
 
 
 def main():
@@ -44,6 +70,7 @@ def main():
 
     try:
         api_error_msg_shorten_link, short_link = shorten_link(VK_SERVICE_TOKEN, link)
+        api_error_msg_count_clicks, clicks_count = count_clicks(VK_SERVICE_TOKEN, short_link)
     except requests.exceptions.HTTPError as e:
         print(f'Exception error: {e}')
         return
@@ -52,7 +79,12 @@ def main():
         print('API error message for shorten link operation:', api_error_msg_shorten_link)
         return
 
+    if api_error_msg_count_clicks:
+        print('API error message for count clicks:', api_error_msg_count_clicks)
+        return
+
     print('Сокращенная ссылка:', short_link)
+    print('Количество кликов по ссылке:', clicks_count)
 
 
 if __name__ == '__main__':

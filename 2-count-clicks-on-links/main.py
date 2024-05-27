cedec72ebb245cd3f4d1ddf4277bd80bf7a1ee22
Mikhail Postnikov <postnikovmu@mail.ru>
2024-05-27 15:41:57 +0300

    test utils.getServerTime

--- 2-count-clicks-on-links/main.py
+++ 2-count-clicks-on-links/main.py
@@ -5,7 +5,18 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-    print(os.environ.get('VK_SERVICE_TOKEN'))
+    VK_SERVICE_TOKEN = os.environ.get('VK_SERVICE_TOKEN')
+    url_template = 'https://api.vk.ru/method/utils.getServerTime'
+    headers = {'Authorization': f'Bearer {VK_SERVICE_TOKEN}'}
+    params = {
+        'v': '5.236'
+    }
+    url = url_template
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    print(response)
+    print(response.text)
+
     pass
 
 

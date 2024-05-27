c391bd3675848528e9f8aaf9dc0ed6d411448b1f
Mikhail Postnikov <postnikovmu@mail.ru>
2024-05-27 17:16:38 +0300

    refactor names fix

--- 2-count-clicks-on-links/main.py
+++ 2-count-clicks-on-links/main.py
@@ -3,44 +3,44 @@ import os
 from dotenv import load_dotenv
 
 
-def shorten_link(token, url):
-    api_url_template = 'https://api.vk.ru/method/utils.getShortLink'
+def shorten_link(token, link):
+    url_template = 'https://api.vk.ru/method/utils.getShortLink'
     headers = {'Authorization': f'Bearer {token}'}
     params = {
         # VK API version
         'v': '5.236',
         # URL to be shortened
-        'url': url,
+        'url': link,
         'private': 0
     }
-    api_url = api_url_template
-    response = requests.get(api_url, headers=headers, params=params)
+    url = url_template
+    response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
 
     print(response)
     print(response.text)
     print(response.json())
     error = ''
-    short_url = ''
+    short_link = ''
     if response.ok and response.json().get("response"):
-        short_url = response.json().get("response").get("short_url")
+        short_link = response.json().get("response").get("short_url")
     if response.ok and response.json().get("error"):
         error = response.json().get("error").get("error_msg")
-    return error, short_url
+    return error, short_link
 
 
 def main():
     load_dotenv()
     VK_SERVICE_TOKEN = os.environ.get('VK_SERVICE_TOKEN')
 
-    #url = 'dvmn.org/modules'
-    url = input()
+    #link = 'dvmn.org/modules'
+    link = input()
 
     error = ''
-    short_url = ''
+    short_link = ''
 
     try:
-        error, short_url = shorten_link(VK_SERVICE_TOKEN, url)
+        error, short_link = shorten_link(VK_SERVICE_TOKEN, link)
     except requests.exceptions.HTTPError as e:
         print(f'Error: {e}')
         return
@@ -49,7 +49,7 @@ def main():
         print('API error:', error)
         return
 
-    print('Сокращенная ссылка:', short_url)
+    print('Сокращенная ссылка:', short_link)
 
 
 if __name__ == '__main__':

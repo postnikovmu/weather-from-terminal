6d179b0d3187ef164e92b791e4cf1dae7c88843d
Mikhail Postnikov <postnikovmu@mail.ru>
2024-05-28 13:32:53 +0300

    debug print fix

--- 2-count-clicks-on-links/main.py
+++ 2-count-clicks-on-links/main.py
@@ -6,50 +6,37 @@ from urllib.parse import urlparse
 
 def is_shorten_link(token, link):
 
-    url_template = 'https://api.vk.ru/method/utils.checkLink'
+    url = 'https://api.vk.ru/method/utils.checkLink'
     headers = {'Authorization': f'Bearer {token}'}
     params = {
-        # VK API version
         'v': '5.236',
-        # URL to be shortened
         'url': link,
     }
-    url = url_template
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
 
-    print(response)
-    print(response.text)
-    print(response.json())
     result_link = ''
     api_error_msg = ''
     if response.ok and response.json().get("response"):
         result_link = response.json().get("response").get("link")
     if response.ok and response.json().get("error"):
         api_error_msg = response.json().get("error").get("error_msg")
-    print(result_link)
     if link in result_link:
         return api_error_msg, False
     return api_error_msg, True
 
 
 def shorten_link(token, link):
-    url_template = 'https://api.vk.ru/method/utils.getShortLink'
+    url = 'https://api.vk.ru/method/utils.getShortLink'
     headers = {'Authorization': f'Bearer {token}'}
     params = {
-        # VK API version
         'v': '5.236',
-        # URL to be shortened
         'url': link,
         'private': 0
     }
-    url = url_template
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
 
-    print(response)
-    print(response.text)
-    print(response.json())
     api_error_msg = ''
     short_link = ''
     if response.ok and response.json().get("response"):
@@ -63,25 +50,18 @@ def count_clicks(token, link):
 
     parsed = urlparse(link)
     key = parsed.path.replace('/', '')
-    print("key:", key)
 
-    url_template = 'https://api.vk.ru/method/utils.getLinkStats'
+    url = 'https://api.vk.ru/method/utils.getLinkStats'
     headers = {'Authorization': f'Bearer {token}'}
     params = {
-        # VK API version
         'v': '5.236',
-        # Shortened link (part of the URL after “vk.cc/”)
         'key': key,
         'interval': 'forever',
         'extended': 0
     }
-    url = url_template
     response = requests.get(url, headers=headers, params=params)
     response.raise_for_status()
 
-    print(response)
-    print(response.text)
-    print(response.json())
     api_error_msg = ''
     clicks_count = 0
     if response.ok and response.json().get("response"):
@@ -95,8 +75,7 @@ def main():
     load_dotenv()
     VK_SERVICE_TOKEN = os.environ.get('VK_SERVICE_TOKEN')
 
-    #link = 'dvmn.org/modules'
-    link = input()
+    link = input('Введите ссылку: ')
 
     short_link = ''
     clicks_count = 0
